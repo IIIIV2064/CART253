145699debe5a176047a4b14679d7d5cb8e999f3b
IIIIV2064 <zdongfang1914@gmail.com>
2020-09-24 12:54:27 -0400

    A3: Finished

--- activities/A2 - Alien/js/script.js
+++ activities/A2 - Alien/js/script.js
@@ -1,20 +1,32 @@
-/**************************************************
-Template p5 project
-Pippin Barr
+//basic values
+h=720
+w=720
 
-Here is a description of this template p5 project.
-**************************************************/
-
-// setup()
-//
-// Description of setup() goes here.
 function setup() {
-
+  createCanvas(h,w);
+  noStroke();
 }
 
-// draw()
-//
-// Description of draw() goes here.
 function draw() {
+  background(0,10,20);
+
+  //face
+  fill(50,205,50);
+  ellipse(h/2,w/2,400,500);
+
+  //mouth
+  fill(0);
+  ellipse(h/2,500,50,50);
+
+  //nose and eyes
+  triangle(h/2,450,h/2-50,400,h/2+50,400);
+
+  triangle(h/2,250,h/2-50,400,h/2+50,400);
+
+  triangle(h/2-20,250,h/2-150,250,h/2-30,275);
+
+  triangle(h/2+20,250,h/2+150,250,h/2+30,275);
+
+
 
 }
--- activities/A3 - Move Pic/js/script.js
+++ activities/A3 - Move Pic/js/script.js
@@ -1,20 +1,59 @@
-/**************************************************
-Template p5 project
-Pippin Barr
+//basic settings
+w=1080
+h=720
+speedm=5
+speedg=2
 
-Here is a description of this template p5 project.
-**************************************************/
+let bgc = {
+r:0,
+g:0,
+b:0,
 
-// setup()
-//
-// Description of setup() goes here.
-function setup() {
+};
 
+let circle1 = {
+  x: 0,
+  y: h/2,
+  size: 200,
+  fill: 255,
+  alpha: 100,
+}
+let circle2 = {
+  x: w,
+  y: h/2,
+  size: 100,
+  fill: 255,
 }
 
-// draw()
-//
-// Description of draw() goes here.
-function draw() {
 
+
+function setup() {
+  createCanvas(w,h);
+    noStroke();
+}
+
+function draw() {
+  //background
+  bgc.r = bgc.r+=1;
+  background(bgc.r,0,0);
+
+
+
+  //circle1
+
+  circle1.size = circle1.size + speedg;
+  circle1.x = circle1.x + speedm;
+  circle1.x = constrain(circle1.x,0,w/2)
+
+  fill(circle1.fill,circle1.alpha);
+  ellipse(circle1.x, circle1.y, circle1.size);
+
+
+  //circle2
+  circle2.size = circle2.size + speedg;
+  circle2.x = circle2.x - speedm;
+  circle2.x = constrain(circle2.x,w/2,w)
+
+  fill(circle2.fill);
+  ellipse(circle2.x, circle2.y, circle2.size);
 }
