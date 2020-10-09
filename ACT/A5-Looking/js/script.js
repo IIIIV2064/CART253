e431308c84bcedaab12e147ff7283337f038c819
IIIIV2064 <zdongfang1914@gmail.com>
2020-10-08 20:23:27 -0400

    A5: Started

---
+++ ACT/A5-Looking/js/script.js
@@ -0,0 +1,25 @@
+/**************************************************
+Activity 05
+Zhao
+
+Working with states: title screen, action screen and end screen for both BE and HE
+**************************************************/
+
+// Creating the objects
+
+
+
+
+
+
+function setup() {
+
+}
+
+// draw()
+
+
+
+function draw() {
+
+}
